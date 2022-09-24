87f5f09801b2ac2ae52b0e20c8eace53af0b6dce
julie <hlee748@gmail.com>
2022-09-24 17:57:43 +0900

    search에 eventTransformer 적용

--- lib/blocs/todo_search/todo_search_bloc.dart
+++ lib/blocs/todo_search/todo_search_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
 
 part 'todo_search_event.dart';
 part 'todo_search_state.dart';
@@ -11,6 +12,10 @@ class TodoSearchBloc extends Bloc<TodoSearchEvent, TodoSearchState> {
   TodoSearchBloc() : super(TodoSearchState.initial()) {
     on<SetSearchTermEvent>((event, emit) {
       emit(state.copyWith(searchTerm: event.newSearchTerm));
-    });
+    }, transformer: debounce(Duration(milliseconds: 2000)));
+  }
+
+  EventTransformer<SetSearchTermEvent> debounce<SetSearchTermEvent>(Duration duration){
+    return(events, mapper) => events.debounceTime(duration).flatMap(mapper);
   }
 }
--- lib/pages/search_and_filter_todo.dart
+++ lib/pages/search_and_filter_todo.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_cubit/cubits/cubits.dart';
-import 'package:todo_cubit/utils/debounce.dart';
 
 import '../blocs/blocks.dart';
 import '../models/todo_model.dart';
 
 class SearchAndFilterTodo extends StatelessWidget {
   SearchAndFilterTodo({Key? key}) : super(key: key);
-  final debounce = Debounce(milliseconds: 1000);
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +20,8 @@ class SearchAndFilterTodo extends StatelessWidget {
           ),
           onChanged: (String? newSearchTerm){
             if (newSearchTerm != null) {
-              debounce.run((){
                 context.read<TodoSearchBloc>().add(SetSearchTermEvent(newSearchTerm: newSearchTerm));
-              });
+
             }
           },
         ),
